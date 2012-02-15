63a815764b96a7f077f10bae3279e678822c110d
Mathias Rav <rav@cs.au.dk>
2012-02-15 21:35:09 +0100

    New deterministic input generator

--- Makefile
+++ Makefile
@@ -9,7 +9,7 @@ opt: opt.o rate.o solve.o
 test: test.o solve.o
 	$(CXX) $(LDFLAGS) $(CXXFLAGS) $(LIBS) -o $@ $^
 
-geninput: geninput.o solve.o
+geninput: geninput.o
 	$(CXX) $(LDFLAGS) $(CXXFLAGS) $(LIBS) -o $@ $^
 
 %.o: %.cpp
--- geninput.cpp
+++ geninput.cpp
@@ -1,20 +1,32 @@
 #include <iostream>
 #include <vector>
-#include "solve.h"
+
+const int floors = 4;
+const int people[] = {7, 12, 13, 14};
+const int total_capacity = 80;
+const int destinations = 24;
+
+void print_destinations() {
+    std::cout << "2 2 3 3 4 4 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9" << std::endl;
+}
+
+void print_priorities() {
+    int offset = 0;
+    for (int f = 0; f < floors; ++f) {
+	for (int p = 0; p < people[f]; ++p) {
+	    for (int q = 0; q < people[f]; ++q) {
+		if (q != p) std::cout << offset+q << ' ';
+	    }
+	    std::cout << '\n';
+	}
+	offset += people[f];
+    }
+    std::cout << std::flush;
+}
 
 int main(int argc, char ** argv) {
-    const int n = 20;
-    for (int i = 0; i < n; ++i) {
-	std::vector<int> priorities(n);
-	for (int j = 0; j < n; ++j) {
-	    priorities[j] = j;
-	}
-	shuffle(priorities.begin(), priorities.end());
-	for (int j = 0; j < n; ++j) {
-	    std::cout << priorities[j] << ' ';
-	}
-	std::cout << std::endl;
-    }
+    print_destinations();
+    print_priorities();
     return 0;
 }
 // vim: set sw=4 sts=4 ts=8 noet:
