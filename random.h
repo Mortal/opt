f89925b595d03502f03e54673509952761bb5aea
Mathias Rav <rav@cs.au.dk>
2012-02-16 13:28:12 +0100

    Add shuffled_iterator

--- random.h
+++ random.h
@@ -17,5 +17,33 @@ void shuffle(IT begin, IT end) {
     }
 }
 
+template <typename C>
+struct shuffled_iterator {
+    shuffled_iterator(const std::vector<C> & source)
+	: items(source)
+	, n(items.size())
+	, indexes(n)
+	, index(0)
+    {
+	for (size_t i = 0; i < n; ++i) {
+	    indexes[i] = i;
+	}
+	std::iter_swap(items.begin(), items.begin()+rand_less_than(n));
+    }
+    const C & operator*() {
+	return items[index];
+    }
+    void operator++(int) { ++*this; }
+    void operator++() {
+	++index;
+	std::iter_swap(items.begin()+index, items.begin()+index+rand_less_than(n)-1);
+    }
+private:
+    std::vector<C> items;
+    size_t n;
+    std::vector<int> indexes;
+    size_t index;
+};
+
 #endif // __RANDOM_H__
 // vim: set sw=4 sts=4 ts=8 noet:
