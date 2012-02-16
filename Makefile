3826fb90a9994e154e7fce08ee5268c24cf45572
Mathias Rav <rav@cs.au.dk>
2012-02-16 22:30:38 +0100

    Move RNG functions into same object as opt

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CXX=g++
 CXXFLAGS=-O3 -Wall -Wextra --std=gnu++0x
 
-opt: opt.o random.o
+opt: opt.o
 	$(CXX) $(LDFLAGS) $(CXXFLAGS) $(LIBS) -o $@ $^
 
 all: opt test geninput
@@ -18,8 +18,6 @@ geninput: geninput.o
 opt.o: opt.cpp types.h objective.h solve.h io.h assignment.h
 
 geninput.o: geninput.cpp
-
-random.o: random.cpp random.h
 
 test.o: test.cpp solve.h types.h random.h objective.h assignment.h
 
--- random.h
+++ random.h
@@ -2,6 +2,7 @@
 #define __RANDOM_H__
 
 #include <vector>
+#include <boost/random.hpp>
 
 int rand_less_than(int bound);
 
@@ -44,6 +45,26 @@ private:
     std::vector<int> indexes;
     size_t index;
 };
+
+boost::rand48 random_source;
+
+int rand_less_than(int bound) {
+    boost::uniform_int<> dist(0,bound-1);
+    boost::variate_generator<boost::rand48&, boost::uniform_int<> > rng(random_source, dist);
+    return rng();
+}
+
+bool flip_coin() {
+    boost::uniform_smallint<> dist(0,1);
+    return dist(random_source);
+}
+
+std::vector<char> random_mask(int m, int n) {
+    std::vector<char> mask(n, 0);
+    std::fill(mask.begin(), mask.begin()+m, 1);
+    shuffle(mask.begin(), mask.end());
+    return mask;
+}
 
 #endif // __RANDOM_H__
 // vim: set sw=4 sts=4 ts=8 noet:
