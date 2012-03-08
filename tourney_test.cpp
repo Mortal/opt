d046fc245b127e83b88b0f5a186809319ed56d77
Mathias Rav <rav@cs.au.dk>
2012-03-08 11:50:27 +0100

    Add correctness test

--- tourney_test.cpp
+++ tourney_test.cpp
@@ -21,6 +21,29 @@ int speed_test() {
     return res % 128;
 }
 
+template <size_t n, size_t k>
+int test_correctness() {
+    auto * pq = new std::priority_queue<size_t>();
+    auto * tt = new tournament_tree<size_t, n>();
+    boost::mt19937 rng(time(NULL));
+    for (size_t i = 0; i < n; ++i) {
+	size_t v = rng();
+	pq->push(v);
+	tt->push(v);
+    }
+    size_t bad = 0;
+    for (size_t i = 0; i < k; ++i) {
+	if (pq->top() != tt->top()) ++bad;
+	pq->pop();
+	tt->pop();
+    }
+    if (bad) {
+	std::cout << bad << " bad tops out of " << k << std::endl;
+	return 1;
+    }
+    return 0;
+}
+
 int main(int argc, char ** argv) {
     const static size_t n = 1 << 25;
     const static size_t k = n >> 7;
@@ -36,6 +59,9 @@ int main(int argc, char ** argv) {
     else if (arg == "tourney_speed")
 	return speed_test<tournament_tree<size_t, n>, n, k>();
 
-    std::cout << "Usage: " << argv[0] << " <pq_speed|tourney_speed>" << std::endl;
+    else if (arg == "correctness")
+	return test_correctness<n, k>();
+
+    std::cout << "Usage: " << argv[0] << " <pq_speed|tourney_speed|correctness>" << std::endl;
 }
 // vim: set sw=4 sts=4 ts=8 noet:
