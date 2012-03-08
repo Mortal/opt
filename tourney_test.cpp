7f70a967e21ed1bfbe45c4cdb5473de3c2203019
Mathias Rav <rav@cs.au.dk>
2012-03-08 11:10:30 +0100

    Add tourney vs. pq test for n insertions and k deletions

--- tourney_test.cpp
+++ tourney_test.cpp
@@ -6,25 +6,38 @@
 #include "tourney.h"
 
 template <typename RNG>
-int pq_test(size_t n, RNG & rng) {
+int pq_test(size_t n, size_t k, RNG & rng) {
     boost::timer::auto_cpu_timer t;
     std::priority_queue<size_t> q;
     for (size_t i = 0; i < n; ++i) {
 	q.push(rng());
     }
-    return q.top() % 128;
+    size_t res = 0;
+    for (size_t i = 0; i < k; ++i) {
+	res ^= q.top();
+	q.pop();
+    }
+    return res % 128;
 }
 
-int main(int argc, char ** argv) {
+int main(int, char ** argv) {
     boost::mt19937 rng(time(NULL));
     const static size_t n = 1 << 25;
-    if (std::string(argv[0]) == "./pq_test") return pq_test(n, rng);
+    const static size_t k = n >> 7;
+    // For n = 2^25, k = 2^19, tournament tree fares better.
+    // For n = 2^25, k = 2^18, priority queue fares better.
+    if (std::string(argv[0]) == "./pq_test") return pq_test(n, k, rng);
     boost::timer::auto_cpu_timer t;
     auto * tree = new tournament_tree<size_t, size_t, n>();
     for (size_t i = 0; i < n; ++i) {
 	size_t k = rng();
 	tree->insert(k, k);
     }
-    return tree->winner() % 128;
+    size_t res = 0;
+    for (size_t i = 0; i < k; ++i) {
+	res ^= tree->winner();
+	tree->pop_winner();
+    }
+    return res % 128;
 }
 // vim: set sw=4 sts=4 ts=8 noet:
