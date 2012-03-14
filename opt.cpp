2f28a8259bed764d3ce0b58b8249497a38ebe9c3
Mathias Rav <rav@cs.au.dk>
2012-03-14 21:56:05 +0100

    Add --immediate command line parameter

--- opt.cpp
+++ opt.cpp
@@ -10,19 +10,25 @@
 #include "parallel.h"
 
 template <typename Reporter>
-void go(input_t & input, bool parallel) {
+void go(input_t & input, bool parallel, bool delayed) {
     Reporter reporter;
     obj_goodness objective;
-    if (parallel)
-	parallel_solve<buffer_report_delayed>(input, objective, reporter);
-    else
+    if (parallel) {
+	if (delayed) {
+	    parallel_solve<buffer_report_delayed>(input, objective, reporter);
+	} else {
+	    parallel_solve<buffer_report_immediate>(input, objective, reporter);
+	}
+    } else {
 	solve(input, objective, reporter);
+    }
 }
 
 int main(int argc, char ** argv) {
     bool parallel = true;
     bool use_debug_reporter = false;
     bool csv = false;
+    bool delayed = true;
     for (int i = 1; i < argc; ++i) {
 	std::string arg(argv[i]);
 	if (arg == "-s" || arg == "--sequential") {
@@ -31,14 +37,16 @@ int main(int argc, char ** argv) {
 	    use_debug_reporter = true;
 	} else if (arg == "-c" || arg == "--csv") {
 	    csv = true;
+	} else if (arg == "--immediate") {
+	    delayed = false;
 	}
     }
     input_t input = csv ? csv_parser().parse_csv_input() : get_input();
 
     if (use_debug_reporter)
-	go<debug_reporter>(input, parallel);
+	go<debug_reporter>(input, parallel, delayed);
     else
-	go<cout_reporter>(input, parallel);
+	go<cout_reporter>(input, parallel, delayed);
 
     return 0;
 }
