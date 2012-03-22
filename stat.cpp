4f35e82e038137ba344421869b4e3eb7fa85d540
Mathias Rav <rav@cs.au.dk>
2012-03-22 10:26:32 +0100

    Change stat.cpp to accept the cumulative observations from the normal samples

--- stat.cpp
+++ stat.cpp
@@ -7,34 +7,25 @@
 using namespace std;
 
 int main() {
-    double x;
-    normal_samples xs;
-    normal_samples ys;
-    normal_samples *cur = &xs;
-    string line;
-    while (getline(cin, line)) {
-	stringstream numbers(line);
-	if (!(numbers >> x)) {
-	    if (cur == &ys) break;
-	    cur = &ys;
-	} else {
-	    (*cur)(x);
-	    while (numbers >> x) {
-		(*cur)(x);
-	    }
-	}
-	cout << *cur << endl;
-    }
+    size_t n1, n2;
+    double sum1, uss1, sum2, uss2;
+    std::cin >> n1 >> sum1 >> uss1 >> n2 >> sum2 >> uss2;
+    normal_samples xs(n1, sum1, uss1);
+    normal_samples ys(n2, sum2, uss2);
+    cout << xs << endl << ys << endl;
     pair<double,double> v = common_variance(xs, ys);
     pair<double,double> m = common_mean(xs, ys);
     cout << "\n\nTEST RESULTS\n\nTest of common variance:\n";
     cout << "p_obs = " << fixed << setprecision(2) << v.second*100 << "% => " << ((v.second < 0.05) ? "Rejected" : "Accepted") << '\n';
     if (v.second >= 0.05) {
-	cout << "s^2 = " << v.first << '\n';
+	cout << "sigma^2 <- s^2 = " << v.first << '\n';
 	cout << "Test of common mean:\n";
 	cout << "p_obs = " << fixed << setprecision(2) << m.second*100 << "% => " << ((m.second < 0.05) ? "REJECTED" : "ACCEPTED") << '\n';
 	if (m.second >= 0.05) {
-	    cout << "m = " << m.first << '\n';
+	    cout << "mu <- m = " << m.first << '\n';
+	    normal_samples zs(n1+n2, sum1+sum2, uss1+uss2);
+	    cout << zs << endl;
+	    cout << "Confidence interval: " << zs.ci() << '\n';
 	}
     }
     return 0;
